eb092f47cb4bf490ed90a8d0e16835b413443be3
stellar-seeker-7 <thesevngurl421@gmail.com>
2024-07-01 14:07:13 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('enter-button').addEventListener('click', function() {
+    document.getElementById('overlay').style.display = 'none';
+    document.getElementById('content').style.filter = 'none';
+  });
+  
